6d98805624ad162b605385e64d60830101c8c0e4
Ofer K <ofer.k@tikalk.com>
2016-06-28 16:45:52 +0300

    Basic call by popularity

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,19 @@ var router = express.Router();
 
 var twitter = require('twitter');
 var config = {
-  consumer_key: 'XXXXXXXXXXXXXXXXXXXX',
-  consumer_secret: 'XXXXXXXXXXXXXXXXXXXX',
-  access_token_key: 'XXXXXXXXXXXXXXXXXXXX',
-  access_token_secret: 'XXXXXXXXXXXXXXXXXXXX'
-};
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 
+}; 
+// make a client
+var twitterClient = new twitter(config);
+// pass in the search string, an options object, and a callback
+var options =  { count: 100};
+twitterClient.get('search/tweets', {q: '#haiku',result_type:'popular'}, function(error, tweets, response) {
+    console.log(tweets);
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
